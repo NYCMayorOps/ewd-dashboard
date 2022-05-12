7a27608cfef10994fd4433e3cb5c3ebab35394e8
SteveScott <sscott1@cityops.nyc.gov>
2022-05-12 10:40:31 -0400

    testing ssh

---
+++ scratch.py
@@ -0,0 +1,6 @@
+import os 
+from utils.sharepoint import Sharepoint
+from pathlib import Path
+sp = Sharepoint()
+#
+sp.upload_file(Path(os.getcwd()) / 'mastercard' / 'mastercard_latest.csv', 'Documents/Mastercard', 'mastercard_latest_test.csv', sp.ops )
