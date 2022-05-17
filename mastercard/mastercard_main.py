cf62d58edbd96b1b9f0c43998c5b0dd265a2a093
SteveScott <sscott1@cityops.nyc.gov>
2022-05-17 17:15:19 -0400

    Pull()

--- mastercard/mastercard_main.py
+++ mastercard/mastercard_main.py
@@ -1,7 +1,9 @@
 import mastercard_upload
 import mastercard_backfill
+from mastercard_pull import Pull
 
 if __name__ == '__main__':
+    Pull().pull()
     mastercard_upload.main()
     mastercard_backfill.main()
     print('mastercard main complete')
